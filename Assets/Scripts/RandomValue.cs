cc26066bd51df552036f8e6f648db2d73eef24a3
LeonhardPassini <leonhard.passini03@htl-salzburg.ac.at>
2021-01-25 09:27:48 +0100

    create 3 scenes

---
+++ Assets/Scripts/RandomValue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomValue : MonoBehaviour
+{
+    public float Number;
+    public Text Value;
+
+
+    public void ValueButton()
+    {
+        Number = UnityEngine.Random.value;
+        Value.text = Number.ToString();
+        Debug.Log("Ich werde gedrückt");
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+public void LoadMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void LoadIntroScene()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+
+    public void LoadEndScene()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+}
